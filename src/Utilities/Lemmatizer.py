04ab9e95b51e44709f95c88d268a347076ee6f48
mithrial <bermeit@fim.uni-passau.de>
2015-06-23 18:02:27 +0200

    Comments on lemmatizer

--- src/Utilities/Lemmatizer.py
+++ src/Utilities/Lemmatizer.py
@@ -4,12 +4,24 @@ import nltk
 from nltk.corpus import wordnet
 
 class Lemmatizer(object):
+    """
+    Use for easier lemmatizing of a whole sentence or part of sentence.
+    """
 
     def __init__(self):
         l = nltk.WordNetLemmatizer()
         self.__lemmatize = l.lemmatize
 
     def lemmatize(self, words):
+        """
+        Returns the given sentence or part of sentence where each word is lemmatized.
+        The lemmatizer is the nltk.WordNetLemmatizer and is is pos-tagged before with
+        the default pos-tagger from nltk.pos_tag.
+        Note that the pos-tagger may give wrong results if the sentence is to short or is only one word.
+        :param words: The words to lemmatize
+        :type words: str
+        :return:
+        """
         tokenized = []
         for (word, tag) in nltk.pos_tag(nltk.word_tokenize(words)):
             tokenized.append(self.__lemmatize(
@@ -20,6 +32,14 @@ class Lemmatizer(object):
 
     @staticmethod
     def __get_wordnet_tag(treebank_tag):
+        """
+        Returns the corresponding wordnet tag for the given treebank tag.
+        If it can't determine a wordnet tag, it defaults to wordnet.NOUN.
+        Example: 'NNS' => wordnet.NOUN
+        :param treebank_tag: the treebank tag to convert
+        :type treebank_tag: str
+        :return:
+        """
 
         if treebank_tag.startswith('J'):
             return wordnet.ADJ
