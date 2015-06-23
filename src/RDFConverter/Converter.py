00513ef4c8578b7ab6a977559c6a48842283d6aa
mithrial <bermeit@fim.uni-passau.de>
2015-06-23 18:39:36 +0200

    small reformat

--- src/RDFConverter/Converter.py
+++ src/RDFConverter/Converter.py
@@ -20,22 +20,27 @@ class Converter(object):
         graph = Graph()
         for _, row in data_frame.iterrows():
             bnode = BNode()
+
             # Confidence
             if not pd.isnull(row['Confidence']):
                 graph.add((bnode, self.namespace.extraction_confidence, Literal(row['Confidence'])))
+
             # Context
             if not pd.isnull(row['Context']):
                 graph.add((bnode, self.namespace.context, Literal(row['Context'])))
+
             # Subject
             uri = urllib.quote(row['Subject'], '')
             node = URIRef(uri)
             graph.add((bnode, RDF.subject, node))
             graph.add((node, RDFS.label, Literal(row['Subject'])))
+
             # Predicate
             uri = urllib.quote(row['Predicate'], '')
             node = URIRef(uri)
             graph.add((bnode, RDF.predicate, node))
             graph.add((node, RDFS.label, Literal(row['Predicate'])))
+
             # Predicate Lemmatized
             uri = urllib.quote(row['PredicateLemma'], '')
             node = URIRef(uri)
@@ -47,6 +52,7 @@ class Converter(object):
             node = URIRef(uri)
             graph.add((bnode, RDF.object, node))
             graph.add((node, RDFS.label, Literal(row['Object'])))
+
             # Additional Args
             if not pd.isnull(row['Other_arg']):
                 for arg in row['Other_arg'].split(SUB_SEPARATOR):
@@ -54,10 +60,12 @@ class Converter(object):
                     node = URIRef(uri)
                     graph.add((bnode, self.namespace.argument, node))
                     graph.add((node, RDFS.label, Literal(arg)))
+
             # Temporal
             if not pd.isnull(row['Temporal']):
                 for arg in row['Temporal'].split(SUB_SEPARATOR):
                     graph.add((bnode, self.namespace.temporal, Literal(arg)))
+
             # Spatial
             if not pd.isnull(row['Spatial']):
                 for arg in row['Spatial'].split(SUB_SEPARATOR):
